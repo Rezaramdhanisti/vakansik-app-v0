d108fb1915ace3aeb7d3a55d0b7dd231f1e10f0e
rezarr <rezaramdhanisti@gmail.com>
2025-07-17 11:56:40 +0700

    Build iOS and Android done

--- ios/Vakansik/AppDelegate.swift
+++ ios/Vakansik/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MyTravel",
+      withModuleName: "Vakansik",
       in: window,
       launchOptions: launchOptions
     )
